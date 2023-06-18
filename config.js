f2366582d1ff92849ed734f6e3911ab833f44f1d
Adisdzaky <dzakyadis9@gmail.com>
2023-06-18 15:59:55 +0700

    Scbot Respon Polling

---
+++ config.js
@@ -0,0 +1,50 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://api.zahwazein.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://api.zahwazein.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6285877276864']
+global.premium = ['6285877276864']
+global.packname = 'Sc With PediaBotz'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Jadikan bot sebagai admin terlebih dahulu! 🙏',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Creator pengguna Nomor Bot',
+    wait: '⏳ Mohon tunggu sebentar...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 20
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
